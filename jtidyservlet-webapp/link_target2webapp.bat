37d3775c0a06c9a3f2d11fb4085cd94da9f656bb
vlads <vlads@58265ab0-d70d-0410-8993-b38ad8f1ef24>
2004-11-30 04:32:07 +0000

    remove links then pause
    git-svn-id: https://jtidy.svn.sourceforge.net/svnroot/jtidy/trunk@757 58265ab0-d70d-0410-8993-b38ad8f1ef24

--- jtidyservlet-webapp/link_target2webapp.bat
+++ jtidyservlet-webapp/link_target2webapp.bat
@@ -1,14 +1,22 @@
 @rem This is used to create NTFS Links so webapp could be run directly from sources and classes compiled using Eclipse
-
 @rem http://www.sysinternals.com/ntw2k/source/misc.shtml#junction
 @rem Usage: junction [-d] <junction directory> [<junction target>]
+
+@set servlet_classes=src\webapp\WEB-INF\classes\org\w3c\tidy\servlet
+
+echo removing junctions
+
+junction -d src\webapp\WEB-INF\lib
+junction -d src\webapp\WEB-INF\classes
+junction -d %servlet_classes%\sample
+pause
 
 junction src\webapp\WEB-INF\lib target\jtidyservlet-webapp\WEB-INF\lib
 
 @rem junction src\webapp\WEB-INF\classes target\classes
 
 
-@set servlet_classes=src\webapp\WEB-INF\classes\org\w3c\tidy\servlet
+
 @rem mkdir %classes%
 
 @rem all jtidyservlet classes
